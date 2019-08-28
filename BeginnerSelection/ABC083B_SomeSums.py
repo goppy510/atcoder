726c65fec3eef72fd149899af2f04278b3075e2c
goppy510 <tacumin.goppy.06@vesta.ocn.ne.jp>
2019-08-28 09:43:41 +0900

    first commit

---
+++ BeginnerSelection/ABC083B_SomeSums.py
@@ -0,0 +1,22 @@
+X = list(map(int, input().split()))
+N = X[0]
+A = X[1]
+B = X[2]
+
+total = []
+a = 0
+for i in range((N+1)):
+  b = i
+  lst = []
+  if i >= A and i <= B:
+    total.append(i)
+  else:
+    while i > 0:
+      lst.append(i%10)
+      i //= 10
+    lst.reverse()
+    t = sum(int(j) for j in lst)
+    if t >= A and t <= B:
+      total.append(b)
+a = sum(int(k) for k in total)
+print(a)
