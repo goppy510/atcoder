ca45e92dc15dfcb696307d0d18a31e601505404d
Takumi Goto <34117290+goppy510@users.noreply.github.com>
2019-11-27 22:28:45 +0900

    Create ABC030_A.py

---
+++ BeginnerSelection/ABC030_A.py
@@ -0,0 +1,12 @@
+a,b,c,d = map(int, input().split())
+
+takahashi = b/a
+aoki = d/c
+
+if takahashi > aoki:
+  print('TAKAHASHI')
+elif takahashi == aoki:
+  print('DRAW')
+else:
+  print('AOKI')
+  
