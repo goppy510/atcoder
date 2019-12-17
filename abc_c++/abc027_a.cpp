42e8b8c12d844ad8171f77ffc38583e1d845a489
Takumi Goto <34117290+goppy510@users.noreply.github.com>
2019-12-17 13:30:51 +0900

    Create abc027_a.cpp

---
+++ abc_c++/abc027_a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repe(i, n) for (int i = 0; i <= (n); ++i)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+typedef long long ll;
+const int INF = 1e9;
+const ll mod = 1000000007;
+int gcd(int a,int b){return b?gcd(b,a%b):a;}
+
+int main() {
+    int h,m;
+    cin >> h >> m;
+    int e = 18*60;
+    int t = h*60+m;
+    cout << e-t << "\n";
+}
