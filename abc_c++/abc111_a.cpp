5a68ea228778302222465307ba87bdf8dd4d3e7b
Takumi Goto <34117290+goppy510@users.noreply.github.com>
2019-12-17 21:52:16 +0900

    Create abc111_a.cpp

---
+++ abc_c++/abc111_a.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repe(i, n) for (int i = 0; i <= (n); ++i)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+typedef long long ll;
+const int INF = 1e9;
+const ll mod = 1000000007;
+int gcd(int a,int b){return b?gcd(b,a%b):a;}
+
+int main() {
+  string s,res;
+  cin >> s;
+  for (int i=0; i<s.size(); ++i) {
+    if (s[i] == '1') {
+      res += '9';
+    } else {
+      res += '1';
+    }
+  }
+  cout << res << "\n";
+}
