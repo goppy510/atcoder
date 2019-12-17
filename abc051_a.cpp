f57d3816a752266871fb45e0919df86d43417f26
Takumi Goto <34117290+goppy510@users.noreply.github.com>
2019-12-17 23:06:02 +0900

    Create abc051_a.cpp

---
+++ abc051_a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repe(i, n) for (int i = 0; i <= (n); ++i)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+typedef long long ll;
+const int INF = 1e9;
+const ll mod = 1000000007;
+int gcd(int a,int b){return b?gcd(b,a%b):a;}
+
+int main() {
+  string s,ss,sss,ssss;
+  cin >> s;
+  rep(i, s.size()) {
+    if (i <= 4) {
+      ss += s[i];
+    }
+    if (i >= 6 && i <= 12) {
+      sss += s[i];
+    }
+    if (i >= 14) {
+      ssss += s[i];
+    }
+  }
+  cout << ss << " " << sss << " " << ssss << "\n";
+}
