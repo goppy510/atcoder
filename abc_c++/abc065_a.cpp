9e6dfafcd1b483724764bafef7fbe267325dfa75
Takumi Goto <34117290+goppy510@users.noreply.github.com>
2019-12-18 17:36:03 +0900

    Create abc065_a.cpp

---
+++ abc_c++/abc065_a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repe(i, n) for (int i = 0; i <= (n); ++i)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+typedef long long ll;
+const int INF = 1e9;
+const ll mod = 1000000007;
+int gcd(int a,int b){return b?gcd(b,a%b):a;}
+int lcm(int a,int b){return a*b / gcd(a,b);}
+
+int main() {
+    int x,a,b;
+    cin >> x >> a >> b;
+    if (b-a >= x+1) {
+        cout << "dangerous" << "\n";
+    }
+    if (b-a <= x && b > a) {
+        cout << "safe" << "\n";
+    }
+    if (b <= a) {
+        cout << "delicious" << "\n";
+    }
+}
